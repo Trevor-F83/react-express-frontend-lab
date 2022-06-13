03e3d5222336e61dcbe0e095a9f341f3fde51ad5
trevorfaivre <trevorfaivre@hotmail.com>
2022-06-13 15:33:55 -0500

    set state

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,7 @@
 
+// react core functionality
+import { useState, useEffect } from 'react';
+
 // component libraries
 import { Route } from 'react-router-dom';
 
@@ -7,6 +10,21 @@ import Index from '../pages/Index.js';
 import Show from '../pages/Show.js';
 
 const Main = (props) => {
+
+    const [ people, setPeople ] = useState([]);
+
+    const URL = 'http://localhost:4000/people'
+
+    const getPeople = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    }
+
+    useEffect(() => {
+        getPeople();
+    }, []);
+
     return (
         <main>
             <Route exact path="/">
